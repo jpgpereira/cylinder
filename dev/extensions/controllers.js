b942943fccf6fcdac6bb307d6f065119ea91b228
Luis Soares <luis.soares@comon.pt>
2016-05-12 10:20:14 +0100

    Documenting "controllers" extension

--- dev/extensions/controllers.js
+++ dev/extensions/controllers.js
@@ -7,9 +7,40 @@ module.exports = function (instance) {
 	var initialized = false;
 	var controllers = {}; // controllers!
 
-	// this method allows for controllers to be indexed to the framework!
-	// the controller will be added to a cache until the framework is initialized.
-	// when or if it is initialized, then the module will be initialized as well.
+	/**
+	 * Extends the framework with a specific named controller.<br />
+	 * The controller's constructor won't be initialized until <code>initControllers()</code> is called.
+	 * When, or if, <code>initControllers()</code> is called, then the module will be added as well.<br /><br />
+	 * The controller itself will be added to the internal controller list
+	 * (accessible through <code>controllers()</code>), but it will not be added to the global scope.
+	 *
+	 * @param  {String}   name - The controller's name.
+	 * @param  {Function} func - The controller's constructor.
+	 * @return {Mixed} Returns the result of 'func' after evaluated.
+	 *
+	 * @example
+	 * Cylinder.controller('myctrl', function (cl, controller) {
+	 *     // you can add the controller to the global scope, if you want.
+	 *     // this way, you'll be able to access it easily.
+	 *     window.MyCtrl = controller;
+	 *
+	 *     // controller logic goes here!
+	 *     controller.alert = function (str) {
+	 *         alert('abc');
+	 *     };
+	 *
+	 *     // always return the parent variable itself in the end,
+	 *     // because this is what Cylinder will recognize as the controller to add.
+	 *     return controller;
+	 * });
+	 *
+	 * // you now have two ways to access controllers,
+	 * // either from Cylinder's own method...
+	 * Cylinder.controllers().myctrl.alert('hello!');
+	 *
+	 * // or through the global scope, like we added up.
+	 * MyCtrl.alert('hello, world!');
+	 */
 	instance.controller = function (name, ctor) {
 		// check if we have a name and if it is a string!
 		// if the name is blank, then forget about it and throw error!
@@ -32,8 +63,10 @@ module.exports = function (instance) {
 		return controllers[name].instance; // and return the module itself!
 	};
 
-	// this method just allows us to have a list of indexed controllers!
-	// it'll fetch the controller cache and then add the real object to it!
+	/**
+	 * Returns a list of existing controllers.
+	 * @return {Array}
+	 */
 	instance.controllers = function () {
 		var result = {};
 		_.each(controllers, function (ctrl, name) {
@@ -42,8 +75,13 @@ module.exports = function (instance) {
 		return result;
 	};
 
-	// this method initializes all controllers!
-	// it runs through all modules and initializes everything!
+	/**
+	 * Properly initializes Cylinder's controllers.<br />
+	 * This method is based on jQuery's <code>$(document).ready()</code> shorthand.
+	 *
+	 * @param  {Function} [callback] - Function to run after initialization.
+	 * @return {CylinderClass} Returns the instance itself.
+	 */
 	instance.initControllers = function (callback) {
 		if (initialized) return instance; // don't do a thing if this already ran!
 		initialized = true; // tell the framework that we're set up and ready to go!
